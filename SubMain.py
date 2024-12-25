fcb71be5204b3c6f4767e1158111119d282ff187
Hussam ElNemr <hassom20042019@gmail.com>
2024-12-25 02:51:42 +0200

    SubMain souper

--- SubMain.py
+++ SubMain.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import urllib.parse
+from Souper import souper
+
 def get_work_page(movie_query_name):
     encoded_query_name = urllib.parse.quote(movie_query_name)
     search_page = f"https://www.themoviedb.org/search?query={encoded_query_name}"
